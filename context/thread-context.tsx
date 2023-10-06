af47a57f67295fb1906f8c02a18ed6956281db71
kristopher recinos <kristopher.recinos@gmail.com>
2023-10-06 16:06:26 -0400

    init commit

---
+++ context/thread-context.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState, PropsWithChildren } from "react";
+import { Thread } from "../types/threads";
+import { generateThreads } from "../utils/generate-dommy-data";
+
+export const ThreadContext = React.createContext<Thread[]>([]);
+
+export const ThreadProvdier = ({
+	children,
+}: PropsWithChildren): JSX.Element => {
+	const [threads, setThreads] = useState<Thread[]>([]);
+
+	useEffect(() => {
+		setThreads(generateThreads());
+	}, []);
+
+	return (
+		<ThreadContext.Provider value={threads}>{children}</ThreadContext.Provider>
+	);
+};
